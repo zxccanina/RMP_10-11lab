a3b9a64c0532b6935bc2960b2718b880fb97dad1
ВашÐcokes238 <_cokes238@gmail.com>
2026-03-18 13:55:40 +0300

    Initial commit

---
+++ app/src/main/java/com/meshcheryakov/student_material_design/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.meshcheryakov.student_material_design
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.meshcheryakov.student_material_design.ui.theme.Student_Material_DesignTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Student_Material_DesignTheme {
+
+            }
+        }
+    }
+}
